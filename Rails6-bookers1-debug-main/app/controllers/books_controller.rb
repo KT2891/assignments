a628098c29d1d2799715015a6b84f64e98660e0f
Keishiro Takata <t.k.6.2.1995@gmail.com>
2023-10-07 07:08:52 +0000

    [finish] 1

---
+++ Rails6-bookers1-debug-main/app/controllers/books_controller.rb
@@ -0,0 +1,50 @@
+class BooksController < ApplicationController
+  def top
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ Rails6-bookers1-debug-main/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+
+  root "homes#top"
+  resources :books
+end
